32c599a06f3a4481e94ffc4c5719b73b70961a25
Xxyay62 <yanisthuillet33@gmail.com>
2025-12-29 16:50:01 +0100

    Ajout de fichiers

---
+++ scripts/AD script UO.ps1
@@ -0,0 +1,55 @@
+﻿# Définir le nouveau domaine
+$nouveauDC = "DC=entreprise2,DC=local"
+
+# Liste des OU à créer
+$ouList = @(
+    "OU=Domain Controllers,$nouveauDC",
+    "OU=Utilisateurs,OU=Client AuditMe,$nouveauDC",
+    "OU=Groupes,OU=Client AuditMe,$nouveauDC",
+    "OU=Ressources Humaines,OU=Utilisateurs,OU=Client AuditMe,$nouveauDC",
+    "OU=Service Informatique,OU=Utilisateurs,OU=Client AuditMe,$nouveauDC",
+    "OU=Administratifs,OU=Utilisateurs,OU=Client AuditMe,$nouveauDC",
+    "OU=Marketing,OU=Utilisateurs,OU=Client AuditMe,$nouveauDC",
+    "OU=Comptabilité,OU=Utilisateurs,OU=Client AuditMe,$nouveauDC",
+    "OU=Client AuditMe,$nouveauDC",
+    "OU=Ordinateurs,OU=Client AuditMe,$nouveauDC",
+    "OU=GLocales,OU=Groupes,OU=Client AuditMe,$nouveauDC",
+    "OU=GGlobales,OU=Groupes,OU=Client AuditMe,$nouveauDC",
+    "OU=Employés,OU=Administratifs,OU=Utilisateurs,OU=Client AuditMe,$nouveauDC",
+    "OU=Responsables,OU=Administratifs,OU=Utilisateurs,OU=Client AuditMe,$nouveauDC",
+    "OU=Employés,OU=Comptabilité,OU=Utilisateurs,OU=Client AuditMe,$nouveauDC",
+    "OU=Responsables,OU=Comptabilité,OU=Utilisateurs,OU=Client AuditMe,$nouveauDC",
+    "OU=Employés,OU=Marketing,OU=Utilisateurs,OU=Client AuditMe,$nouveauDC",
+    "OU=Responsables,OU=Marketing,OU=Utilisateurs,OU=Client AuditMe,$nouveauDC",
+    "OU=Employés,OU=Ressources Humaines,OU=Utilisateurs,OU=Client AuditMe,$nouveauDC",
+    "OU=Responsables,OU=Ressources Humaines,OU=Utilisateurs,OU=Client AuditMe,$nouveauDC",
+    "OU=Techniciens,OU=Service Informatique,OU=Utilisateurs,OU=Client AuditMe,$nouveauDC"
+)
+
+# Création des OU
+foreach ($ou in $ouList) {
+    New-ADOrganizationalUnit -Name ($ou -split ",")[0].Replace("OU=", "") -Path ($ou -replace "^OU=[^,]+,", "") -ErrorAction SilentlyContinue
+}
+
+# Mot de passe commun
+$pwd = ConvertTo-SecureString "Sio1234*" -AsPlainText -Force
+
+# Création des utilisateurs
+$users = @(
+    @{Name="Administrateur"; Sam="Administrateur"; Path="OU=Service Informatique,OU=Utilisateurs,OU=Client AuditMe,$nouveauDC"},
+    @{Name="User1"; Sam="User1"; Path="OU=Employés,OU=Administratifs,OU=Utilisateurs,OU=Client AuditMe,$nouveauDC"},
+    @{Name="User2"; Sam="User2"; Path="OU=Responsables,OU=Administratifs,OU=Utilisateurs,OU=Client AuditMe,$nouveauDC"},
+    @{Name="User3"; Sam="User3"; Path="OU=Employés,OU=Comptabilité,OU=Utilisateurs,OU=Client AuditMe,$nouveauDC"},
+    @{Name="User4"; Sam="User4"; Path="OU=Responsables,OU=Comptabilité,OU=Utilisateurs,OU=Client AuditMe,$nouveauDC"},
+    @{Name="User5"; Sam="User5"; Path="OU=Employés,OU=Marketing,OU=Utilisateurs,OU=Client AuditMe,$nouveauDC"},
+    @{Name="User6"; Sam="User6"; Path="OU=Responsables,OU=Marketing,OU=Utilisateurs,OU=Client AuditMe,$nouveauDC"},
+    @{Name="User7"; Sam="User7"; Path="OU=Employés,OU=Ressources Humaines,OU=Utilisateurs,OU=Client AuditMe,$nouveauDC"},
+    @{Name="User8"; Sam="User8"; Path="OU=Responsables,OU=Ressources Humaines,OU=Utilisateurs,OU=Client AuditMe,$nouveauDC"},
+    @{Name="User9"; Sam="User9"; Path="OU=Techniciens,OU=Service Informatique,OU=Utilisateurs,OU=Client AuditMe,$nouveauDC"},
+    @{Name="User10"; Sam="User10"; Path="OU=Administrateurs,OU=Service Informatique,OU=Utilisateurs,OU=Client AuditMe,$nouveauDC"}
+)
+
+foreach ($u in $users) {
+    New-ADUser -Name $u.Name -SamAccountName $u.Sam -Path $u.Path -AccountPassword $pwd -Enabled $true -ErrorAction SilentlyContinue
+}
+
---
+++ scripts/script Groupes AD.ps1
@@ -0,0 +1,72 @@
+﻿# Définir le chemin des OU
+$pathGG = "OU=GGlobales,OU=Groupes,OU=Client AuditMe,DC=entreprise2,DC=local"
+$pathGL = "OU=GLocales,OU=Groupes,OU=Client AuditMe,DC=entreprise2,DC=local"
+
+$ggList = @(
+    "Gg_Service_Informatique",
+    "Gg_Comptabilité",
+    "Gg_Marketing",
+    "Gg_Administratifs",
+    "Gg_Ressources_Humaines",
+    "Gg_Administrateurs_Informatiques",
+    "Gg_Techniciens_informatiques",
+    "Gg_Comptabilité_Employés",
+    "Gg_Comptabilité_Responsables",
+    "Gg_Marketing_responsables",
+    "Gg_Marketing_Employés",
+    "Gg_Administratifs_Responsables",
+    "Gg_Administratifs_Employés",
+    "Gg_Ressources_Humaines_Responsables",
+    "Gg_Ressources_Humaines_Employés"
+)
+
+foreach ($gg in $ggList) {
+    New-ADGroup -Name $gg -GroupScope Global -GroupCategory Security -Path "OU=GGlobales,OU=Groupes,OU=Client AuditMe,DC=entreprise2,DC=local" -ErrorAction SilentlyContinue
+}
+
+$glList = @(
+    "DL_Echanges_LM",
+    "DL_Echanges_L",
+    "DL_Echanges_CT",
+    "DL_RH_LM",
+    "DL_RH_L",
+    "DL_RH_CT",
+    "DL_SI_LM",
+    "DL_SI_CT",
+    "DL_ADM_LM",
+    "DL_ADM_CT",
+    "DL_MARK_LM",
+    "DL_MARK_L",
+    "DL_ADM_CT2",
+    "DL_COM_LM",
+    "DL_COM_CT"
+)
+
+foreach ($gl in $glList) {
+    New-ADGroup -Name $gl -GroupScope DomainLocal -GroupCategory Security -Path "OU=GLocales,OU=Groupes,OU=Client AuditMe,DC=entreprise2,DC=local" -ErrorAction SilentlyContinue
+}
+
+# Affectations selon le tableau
+$affectations = @{
+    "DL_Echanges_LM" = @("Gg_Service_Informatique", "Gg_Comptabilité", "Gg_Marketing", "Gg_Administratifs", "Gg_Ressources_Humaines")
+    "DL_Echanges_L"  = @("Gg_Comptabilité", "Gg_Marketing", "Gg_Administratifs", "Gg_Ressources_Humaines")
+    "DL_Echanges_CT" = @("Gg_Comptabilité", "Gg_Marketing", "Gg_Administratifs", "Gg_Ressources_Humaines")
+    "DL_RH_LM"        = @("Gg_Ressources_Humaines", "Gg_Ressources_Humaines_Responsables", "Gg_Ressources_Humaines_Employés")
+    "DL_RH_L"         = @("Gg_Ressources_Humaines", "Gg_Ressources_Humaines_Responsables", "Gg_Ressources_Humaines_Employés")
+    "DL_RH_CT"        = @("Gg_Ressources_Humaines", "Gg_Ressources_Humaines_Responsables", "Gg_Ressources_Humaines_Employés")
+    "DL_SI_LM"        = @("Gg_Service_Informatique", "Gg_Administrateurs_Informatiques", "Gg_Techniciens_informatiques")
+    "DL_SI_CT"        = @("Gg_Service_Informatique", "Gg_Administrateurs_Informatiques", "Gg_Techniciens_informatiques")
+    "DL_ADM_LM"       = @("Gg_Administratifs", "Gg_Administratifs_Responsables", "Gg_Administratifs_Employés")
+    "DL_ADM_CT"       = @("Gg_Administratifs", "Gg_Administratifs_Responsables", "Gg_Administratifs_Employés")
+    "DL_MARK_LM"      = @("Gg_Marketing", "Gg_Marketing_responsables", "Gg_Marketing_Employés")
+    "DL_MARK_L"       = @("Gg_Marketing", "Gg_Marketing_responsables", "Gg_Marketing_Employés")
+    "DL_ADM_CT2"      = @("Gg_Administratifs_Responsables", "Gg_Administratifs_Employés")
+    "DL_COM_LM"       = @("Gg_Comptabilité", "Gg_Comptabilité_Responsables", "Gg_Comptabilité_Employés")
+    "DL_COM_CT"       = @("Gg_Comptabilité", "Gg_Comptabilité_Responsables", "Gg_Comptabilité_Employés")
+}
+
+foreach ($gl in $affectations.Keys) {
+    foreach ($gg in $affectations[$gl]) {
+        Add-ADGroupMember -Identity $gl -Members $gg -ErrorAction SilentlyContinue
+    }
+}
